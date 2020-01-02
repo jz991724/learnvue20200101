205a03f125ca9104430ffd4bbe31d5c3d6ca7d4a
zhangjun <1548293573@qq.com>
2020-01-02 14:10:59 +0800

    20200101提交

--- src/components/HellowVue.vue
+++ src/components/HellowVue.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <a-button type="danger">提交</a-button>
+        <a-button type="danger" v-test>提交</a-button>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: "HellowVue"
+    import $ from "jquery";
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component({
+        directives: {
+            test: {
+                bind: function (el) {
+                    let text = $(el).text();
+                    console.log(text);
+                }
+            }
+        },
+        filters:{
+
+        }
+    })
+    export default class test extends Vue {
+        name: string = "HellowVue";
     }
 </script>
 
