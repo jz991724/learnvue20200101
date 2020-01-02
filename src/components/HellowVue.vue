08095fcdaee5d90d22c615b14ad2a6aa026f0cb8
zhangjun <1548293573@qq.com>
2020-01-02 16:25:24 +0800

    20200101提交

--- src/components/HellowVue.vue
+++ src/components/HellowVue.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <a-button type="danger" v-test>提交</a-button>
+        <a-button type="danger" v-test @click="add">提交</a-button>
+        <input type="text" placeholder="请填写姓..." v-model="lastName">
+        <input type="text" placeholder="请填写名..." v-model="firstName">
+        <input v-model="genterName">
+        <br>
+        <div>
+            组件： {{modelTest}}
+        </div>
+        <input :value="modelTest" @change="$emit('testModel',$event.target.value)">
     </div>
 </template>
 
 <script lang="ts">
     import $ from "jquery";
-    import {Vue, Component} from "vue-property-decorator";
+    import {Vue, Component, PropSync, Prop, Watch, Model} from "vue-property-decorator";
 
     @Component({
         directives: {
@@ -17,12 +25,36 @@
                 }
             }
         },
-        filters:{
-
+        filters: {
+            test(val: string) {
+                return val + ",哈哈哈哈哈";
+            }
         }
     })
     export default class test extends Vue {
-        name: string = "HellowVue";
+        lastName: string = "";
+        firstName: string = "";
+        name: string = "";
+
+        get genterName() {
+            this.name = this.lastName + this.firstName;
+            return this.name;
+        }
+
+        @Prop({type: Number, default: -1}) count!: Number;
+
+        add() {
+            this.$emit("test", this.count);
+        }
+
+        @Watch('name')
+        changeName(newVal: string, oldVal: string) {
+            if (newVal !== oldVal) {
+                alert(newVal);
+            }
+        }
+
+        @Model("testModel", {type: String, default: "哈哈哈"}) modelTest!: string;
     }
 </script>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
     <h1>这就是一个Demo</h1>
-    <hellow-vue></hellow-vue>
+    <hellow-vue @test="getChildrenVal" :count="aboutcount" v-model="msg"></hellow-vue>
+    <div>
+      {{msg}}
+    </div>
+    <a-button @click="msg=''">test</a-button>
   </div>
 </template>
 <script lang="ts">
@@ -13,6 +17,10 @@
     }
   })
   export default class test extends  Vue{
-
+    msg:string="";
+    aboutcount:number=20;
+    getChildrenVal(param:number){
+      this.aboutcount=param*2;
+    }
   }
 </script>
